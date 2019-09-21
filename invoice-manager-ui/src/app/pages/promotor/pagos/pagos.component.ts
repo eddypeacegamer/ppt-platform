68fabbcceb74723d8439ee130e7d6badb599d82d
ralfdemoledor <rubens.mncrft@gmail.com>
2019-09-21 14:37:57 -0500

    adding missing mocks to operations and promotor modules

--- invoice-manager-ui/src/app/pages/promotor/pagos/pagos.component.ts
+++ invoice-manager-ui/src/app/pages/promotor/pagos/pagos.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CatalogsData } from '../../../@core/data/catalogs-data';
+import { GenericPage } from '../../../models/generic-page';
+import {DownloadCsvService } from '../../../@core/back-services/download-csv.service'
+
 @Component({
   selector: 'ngx-pagos',
   templateUrl: './pagos.component.html',
   styleUrls: ['./pagos.component.scss']
 })
 export class PagosComponent implements OnInit {
+  
+  public headers: string[] = ['Clave', 'Descripcion', 'Similares', 'Inicio Vigencia'];
+  public page: GenericPage<any> = new GenericPage();
+  public pageSize = '10';
 
-  constructor() { }
+  constructor(private catalogService: CatalogsData,
+    private donwloadService:DownloadCsvService) {}
 
   ngOnInit() {
+    this.updateDataTable();
+  }
+
+  public updateDataTable(currentPage?: number, pageSize?: number) {
+    const pageValue = currentPage || 0;
+    const sizeValue = pageSize || 10;
+    this.catalogService.getAllClavesProductoServicio(pageValue, sizeValue)
+      .subscribe(result => this.page = result);
+  }
+
+  public onChangePageSize(pageSize: number) {
+    this.updateDataTable(this.page.number, pageSize);
   }
 
 }
