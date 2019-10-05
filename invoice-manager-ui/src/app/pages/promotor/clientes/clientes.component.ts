60b09fb1e646ff7acbce78d2199f6bbe5183584e
ralfdemoledor <rubens.mncrft@gmail.com>
2019-10-04 22:59:13 -0500

    small changes

--- invoice-manager-ui/src/app/pages/promotor/clientes/clientes.component.ts
+++ invoice-manager-ui/src/app/pages/promotor/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientsData } from '../../../@core/data/clients-data';
 import { Client } from '../../../models/client';
+import { Contribuyente } from '../../../models/contribuyente';
 
 @Component({
   selector: 'ngx-clientes',
@@ -15,12 +16,26 @@ export class ClientesComponent implements OnInit {
   constructor(private clientService:ClientsData) { }
 
   ngOnInit() {
-    this.clientInfo = new Client();
+    
   }
 
   public buscarClientePorRFC(){
     this.clientService.getClientByRFC(this.rfc)
       .subscribe(data => this.clientInfo = data);
+  }
+
+  public onRegitrarCall(){
+    this.clientInfo = new Client();
+    this.clientInfo.informacionFiscal= new Contribuyente();
+    this.clientInfo.informacionFiscal.rfc= this.rfc;
+  }
+
+  public updateClient(){
+    this.clientService.updateClient(this.clientInfo).subscribe(r=>console.log('upadted',r));
+  }
+
+  public insertClient(){
+    this.clientService.insertNewClient(this.clientInfo).subscribe(r=>console.log('inserted',r));
   }
 
 }
