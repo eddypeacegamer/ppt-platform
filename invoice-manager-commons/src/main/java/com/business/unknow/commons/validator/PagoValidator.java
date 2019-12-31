525c3cff07e872dc842f6464f57898f984809c3c
Edgar Ramos <eej000f@walmart.com>
2019-12-30 21:30:29 -0600

    revision pago fix

--- invoice-manager-commons/src/main/java/com/business/unknow/commons/validator/PagoValidator.java
+++ invoice-manager-commons/src/main/java/com/business/unknow/commons/validator/PagoValidator.java
@@ -8,7 +8,7 @@ public class PagoValidator extends AbstractValidator{
 
 	public void validatePagoCreator(PagoDto currentPagoDto,PagoDto pagoDto) throws InvoiceManagerException {
 		if (currentPagoDto.getUltimoUsuario().equalsIgnoreCase(pagoDto.getUltimoUsuario())
-				|| currentPagoDto.getCreateUser().equalsIgnoreCase(pagoDto.getUltimoUsuario())) {
+				|| currentPagoDto.getCreateUser().equalsIgnoreCase(currentPagoDto.getUltimoUsuario())) {
 			throw new InvoiceManagerException("Mismo usuario no puede actualizar el pago",
 					"La actualizacion del pago no puede ser realizada por el mismo usuario de manera consecutiva",
 					Constants.HTTP_SSTATUS_CONFLICT);
