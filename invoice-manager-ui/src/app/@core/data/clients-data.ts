43caf081c7d9877c1f1a852e5ee0abd49ebc1388
ralfdemoledor <rubens.mncrft@gmail.com>
2020-03-01 21:57:26 -0600

    Fix devolutions error

--- invoice-manager-ui/src/app/@core/data/clients-data.ts
+++ invoice-manager-ui/src/app/@core/data/clients-data.ts
@@ -4,11 +4,13 @@ import { Client } from '../../models/client';
 
 export abstract class ClientsData {
 
-    abstract getClients(page:number,size:number,filterParams?:any) : Observable<GenericPage<Client>>;
+    abstract getClients(page:number,size:number,filterParams?:any): Observable<GenericPage<Client>>;
+
+    abstract getClientsByPromotor(promotor: string) : Observable<Client[]>;
 
     abstract getClientByRFC(rfc:string) : Observable<Client>;
 
-    abstract getClientByName(name:string) : Observable<[Client]>;
+    abstract getClientByName(name:string) : Observable<Client[]>;
 
     abstract insertNewClient(client : Client) : Observable<Client>;
 
