253e17c7981e3a017557823d35a3071594af5369
Edgar Ramos <eej000f@walmart.com>
2020-03-07 11:11:26 -0600

    contabilidad fix

--- invoice-manager-services/src/main/java/com/business/unknow/services/services/executor/TimbradoExecutorService.java
+++ invoice-manager-services/src/main/java/com/business/unknow/services/services/executor/TimbradoExecutorService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.business.unknow.commons.builder.EmailConfigBuilder;
 import com.business.unknow.commons.util.MailHelper;
+import com.business.unknow.enums.LineaEmpresaEnum;
 import com.business.unknow.enums.TipoArchivoEnum;
 import com.business.unknow.model.config.FileConfig;
 import com.business.unknow.model.context.FacturaContext;
@@ -66,33 +67,36 @@ public class TimbradoExecutorService {
 				facturaFileRepository.save(filesMapper.getFacturaFileFromDto(facturaFileDto));
 			}
 		}
-		Client client = clientRepository.findByRfc(context.getFacturaDto().getRfcRemitente())
-				.orElseThrow(() -> new InvoiceManagerException("Error sending the email",
-						String.format("The client %s does not exists", context.getFacturaDto().getRfcEmisor()),
-						HttpStatus.SC_CONFLICT));
-		FacturaFileDto xml = context.getFacturaFilesDto().stream()
-				.filter(a -> a.getTipoArchivo().equals(TipoArchivoEnum.XML.name())).findFirst()
-				.orElseThrow(() -> new InvoiceManagerException("Error getting xml", "No se guardo el xml correctamente",
-						HttpStatus.SC_CONFLICT));
-		FacturaFileDto pdf = context.getFacturaFilesDto().stream()
-				.filter(a -> a.getTipoArchivo().equals(TipoArchivoEnum.PDF.name())).findFirst()
-				.orElseThrow(() -> new InvoiceManagerException("Error getting xml", "No se guardo el xml correctamente",
-						HttpStatus.SC_CONFLICT));
-		EmailConfigBuilder emailBuilder = new EmailConfigBuilder().setEmisor(context.getEmpresaDto().getCorreo())
-				.setPwEmisor(context.getEmpresaDto().getPwCorreo())
-				.setAsunto(String.format("Factura %s", context.getFacturaDto().getFolio()))
-				.addReceptor(client.getCorreoPromotor()).addReceptor(client.getInformacionFiscal().getCorreo())
-				.addReceptor(client.getCorreoContacto())
-				.addArchivo(new FileConfig(TipoArchivoEnum.XML,
-						context.getFacturaDto().getFolio().concat(TipoArchivoEnum.XML.getFormat()), xml.getData()))
-				.addArchivo(new FileConfig(TipoArchivoEnum.PDF,
-						context.getFacturaDto().getFolio().concat(TipoArchivoEnum.PDF.getFormat()), pdf.getData()))
-				.setCuerpo("Su factura timbrada es:");
-		try {
-			mailHelper.enviarCorreo(emailBuilder.build());
-		} catch (InvoiceCommonException e) {
-			new InvoiceManagerException(e.getMessage(), e.getErrorMessage().getDeveloperMessage(),
-					HttpStatus.SC_CONFLICT);
+		if (context.getEmpresaDto().getTipo().equals(LineaEmpresaEnum.A.name())) {
+			Client client = clientRepository.findByRfc(context.getFacturaDto().getRfcRemitente())
+					.orElseThrow(() -> new InvoiceManagerException("Error sending the email",
+							String.format("The client %s does not exists", context.getFacturaDto().getRfcEmisor()),
+							HttpStatus.SC_CONFLICT));
+			FacturaFileDto xml = context.getFacturaFilesDto().stream()
+					.filter(a -> a.getTipoArchivo().equals(TipoArchivoEnum.XML.name())).findFirst()
+					.orElseThrow(() -> new InvoiceManagerException("Error getting xml",
+							"No se guardo el xml correctamente", HttpStatus.SC_CONFLICT));
+			FacturaFileDto pdf = context.getFacturaFilesDto().stream()
+					.filter(a -> a.getTipoArchivo().equals(TipoArchivoEnum.PDF.name())).findFirst()
+					.orElseThrow(() -> new InvoiceManagerException("Error getting xml",
+							"No se guardo el xml correctamente", HttpStatus.SC_CONFLICT));
+			EmailConfigBuilder emailBuilder = new EmailConfigBuilder().setEmisor(context.getEmpresaDto().getCorreo())
+					.setPwEmisor(context.getEmpresaDto().getPwCorreo())
+					.setAsunto(String.format("Factura %s", context.getFacturaDto().getFolio()))
+					.addReceptor(client.getCorreoPromotor()).addReceptor(client.getInformacionFiscal().getCorreo())
+					.addReceptor(client.getCorreoContacto())
+					.addArchivo(new FileConfig(TipoArchivoEnum.XML,
+							context.getFacturaDto().getFolio().concat(TipoArchivoEnum.XML.getFormat()), xml.getData()))
+					.addArchivo(new FileConfig(TipoArchivoEnum.PDF,
+							context.getFacturaDto().getFolio().concat(TipoArchivoEnum.PDF.getFormat()), pdf.getData()))
+					.setCuerpo("Su factura timbrada es:");
+			try {
+				mailHelper.enviarCorreo(emailBuilder.build());
+			} catch (InvoiceCommonException e) {
+				e.printStackTrace();
+				new InvoiceManagerException(e.getMessage(), e.getErrorMessage().getDeveloperMessage(),
+						HttpStatus.SC_CONFLICT);
+			}
 		}
 	}
 
