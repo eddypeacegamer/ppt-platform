c641e47e017f76f4173c407715ccf084019d0ea4
Edgar Ramos <eej000f@walmart.com>
2020-03-09 21:46:28 -0600

    fixing constraint bug

--- invoice-manager-commons/src/main/java/com/business/unknow/commons/util/NumberHelper.java
+++ invoice-manager-commons/src/main/java/com/business/unknow/commons/util/NumberHelper.java
@@ -1,6 +1,7 @@
 package com.business.unknow.commons.util;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class NumberHelper {
 
@@ -8,5 +9,10 @@ public class NumberHelper {
 		BigDecimal tempBig = new BigDecimal(Double.toString(number));
 		return tempBig.setScale(scale, BigDecimal.ROUND_HALF_EVEN).doubleValue();
 	}
+	
+	public BigDecimal assignBigDecimalPrecision(BigDecimal number, int scale) {
+		return  number.setScale(scale, RoundingMode.DOWN);
+	}
+	
 
 }
