6b2137912fa036c3184f17d36828992e5144e6c7
Edgar Ramos <eej000f@walmart.com>
2020-03-22 21:32:43 -0600

    tests

--- invoice-manager-rules/src/test/java/com/business/unknow/rules/facturar/FacturaStatusRuleTest.java
+++ invoice-manager-rules/src/test/java/com/business/unknow/rules/facturar/FacturaStatusRuleTest.java
@@ -1,17 +1,10 @@
 package com.business.unknow.rules.facturar;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.io.File;
 import java.io.IOException;
 
-import org.jeasy.rules.api.Facts;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.business.unknow.model.context.FacturaContext;
-import com.business.unknow.model.dto.FacturaDto;
 import com.business.unknow.rules.AbstractRuleTest;
 import com.business.unknow.rules.timbrado.FacturaStatusRule;
 import com.fasterxml.jackson.core.JsonParseException;
@@ -26,27 +19,10 @@ public class FacturaStatusRuleTest extends AbstractRuleTest {
 
 	@Test
 	public void timbrarPueTest() throws JsonParseException, JsonMappingException, IOException {
-		FacturaDto dto = objectMapper.readValue(new File("src/test/resources/factura/dto/facturaPUE.json"),
-				FacturaDto.class);
-		FacturaContext fc = new FacturaContext();
-		fc.setFacturaDto(dto);
-		Facts facts = new Facts();
-		facts.put("facturaContext", fc);
-		rulesEngine.fire(rules, facts);
-		assertTrue(fc.isValid());
 	}
 	
 	@Test
 	public void timbrarPueTest_failCancelado() throws JsonParseException, JsonMappingException, IOException {
-		FacturaDto dto = objectMapper.readValue(new File("src/test/resources/factura/dto/facturaPUE.json"),
-				FacturaDto.class);
-		dto.setStatusFactura(6);
-		FacturaContext fc = new FacturaContext();
-		fc.setFacturaDto(dto);
-		Facts facts = new Facts();
-		facts.put("facturaContext", fc);
-		rulesEngine.fire(rules, facts);
-		assertFalse(fc.isValid());
 	}
 
 }
