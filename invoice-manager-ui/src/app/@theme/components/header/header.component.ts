785aee88332416411c805d6b6c434363c7b2c6ac
Ralph Demoledor <rubens.mncrft@gmail.com>
2020-04-27 19:43:18 -0500

    Increasing version

--- invoice-manager-ui/src/app/@theme/components/header/header.component.ts
+++ invoice-manager-ui/src/app/@theme/components/header/header.component.ts
@@ -39,7 +39,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   currentTheme = 'default';
 
-  userMenu = [{ title: 'V0.902' }];
+  userMenu = [{ title: 'V0.903' }];
 
   constructor(private sidebarService: NbSidebarService,
     private menuService: NbMenuService,
