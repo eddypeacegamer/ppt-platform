b0bd25ee69789938ab97501c8c6736f2d53c1214
ralfdemoledor <rubens.mncrft@gmail.com>
2020-05-05 20:22:56 -0500

    Adding inactivate/active clients on operations

--- invoice-manager-ui/src/app/pages/administracion/administracion.module.ts
+++ invoice-manager-ui/src/app/pages/administracion/administracion.module.ts
@@ -3,15 +3,13 @@ import { DownloadCsvService } from '../../@core/util-services/download-csv.servi
 import { AdministracionRoutingModule } from './administracion-routing.module';
 import { AdministracionComponent } from './administracion.component';
 import { UsersComponent } from './users/users.component';
-import { DevolutionsDetailsComponent } from '../commons/devolutions-details/devolutions-details.component';
 import { CommonsModule } from '../commons/commons.module';
 
 
 @NgModule({
   declarations: [
     AdministracionComponent,
-    UsersComponent,
-    DevolutionsDetailsComponent],
+    UsersComponent],
   imports: [
     AdministracionRoutingModule,
     CommonsModule,
