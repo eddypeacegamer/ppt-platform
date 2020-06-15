ce13dd57d437eef02665df7a02732b77678e54e5
TortaDosPorUno <torta.dosporuno@gmail.com>
2020-06-14 22:01:44 -0500

    avance vistas cdfi
    animaciones de entrada izquierda, animacion centro titulos targetas
    ajustes a vista cdfi de css
    titulo header responsivo
    loader
    ocultar conceptos tabla en pagos cero

--- invoice-manager-ui/src/app/auth/unauthorized/unauthorized.component.ts
+++ invoice-manager-ui/src/app/auth/unauthorized/unauthorized.component.ts
@@ -9,9 +9,33 @@ import { DomSanitizer, SafeUrl, SafeResourceUrl } from '@angular/platform-browse
 export class UnauthorizedComponent implements OnInit {
 
   safeSrc: SafeResourceUrl;
+  audio = new Audio();
   constructor(private sanitizer: DomSanitizer) { 
+    
+    this.playAudio();
     this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/vI_ejPZkCFo");
   }
+
+  playAudio(){
+    
+    this.audio.src = "/assets/data/palabra.mp3";
+    this.audio.load();
+    //this.audio.play();
+    this.audio.loop = true;
+    this.audio.volume = 0;
+  }
+
+  mute() {
+    if (this.audio.volume !== 0) {
+      this.audio.play();
+      this.audio.volume = 0;
+      
+    } else {
+      this.audio.play();
+      this.audio.volume = 1;
+    }
+  }
+
   ngOnInit() {
   }
 
