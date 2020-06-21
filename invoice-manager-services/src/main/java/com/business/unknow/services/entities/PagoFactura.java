4b7bbc7b08d181394f9cc331d883028c9c527f6a
ralfdemoledor <rubens.mncrft@gmail.com>
2020-06-20 20:46:11 -0500

    Defining pago factura model

--- invoice-manager-services/src/main/java/com/business/unknow/services/entities/PagoFactura.java
+++ invoice-manager-services/src/main/java/com/business/unknow/services/entities/PagoFactura.java
@@ -3,10 +3,21 @@
  */
 package com.business.unknow.services.entities;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 /**
@@ -17,5 +28,93 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "PAGO_FACTURAS")
 public class PagoFactura {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Integer id;
+	
+	@Column(name = "ID_PAGO")
+	private Integer idPago;
+	
+	@Column(name = "ID_CFDI")
+	private Integer idCfdi;
+	
+	@Column(name = "FOLIO")
+	private String folio;
+	
+	@Column(name = "MONTO")
+	private BigDecimal monto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	@Column(name = "FECHA_CREACION")
+	private Date fechaCreacion;
 
+	@Temporal(TemporalType.TIMESTAMP)
+	@LastModifiedDate
+	@Column(name = "FECHA_ACTUALIZACION")
+	private Date fechaActualizacion;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getIdPago() {
+		return idPago;
+	}
+
+	public void setIdPago(Integer idPago) {
+		this.idPago = idPago;
+	}
+
+	public Integer getIdCfdi() {
+		return idCfdi;
+	}
+
+	public void setIdCfdi(Integer idCfdi) {
+		this.idCfdi = idCfdi;
+	}
+
+	public String getFolio() {
+		return folio;
+	}
+
+	public void setFolio(String folio) {
+		this.folio = folio;
+	}
+
+	public BigDecimal getMonto() {
+		return monto;
+	}
+
+	public void setMonto(BigDecimal monto) {
+		this.monto = monto;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Date getFechaActualizacion() {
+		return fechaActualizacion;
+	}
+
+	public void setFechaActualizacion(Date fechaActualizacion) {
+		this.fechaActualizacion = fechaActualizacion;
+	}
+
+	@Override
+	public String toString() {
+		return "PagoFactura [id=" + id + ", idPago=" + idPago + ", idCfdi=" + idCfdi + ", folio=" + folio + ", monto="
+				+ monto + ", fechaCreacion=" + fechaCreacion + ", fechaActualizacion=" + fechaActualizacion + "]";
+	}
 }
