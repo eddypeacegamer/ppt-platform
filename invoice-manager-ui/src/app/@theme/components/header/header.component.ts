c50b33e945fbbcedcba3f8e2a3326f494f9a2105
Edgar Ramos <eej000f@walmart.com>
2020-08-07 10:11:08 -0500

    version

--- invoice-manager-ui/src/app/@theme/components/header/header.component.ts
+++ invoice-manager-ui/src/app/@theme/components/header/header.component.ts
@@ -39,7 +39,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   currentTheme = 'default';
 
-  userMenu = [{ title: 'V2.005' }];
+  userMenu = [{ title: 'V2.006' }];
 
   constructor(private sidebarService: NbSidebarService,
     private menuService: NbMenuService,
