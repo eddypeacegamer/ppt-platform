9d28b6a4579eb19c0c2b7046ea734fa82e86d04e
edgar ramos <eddypeacegamer@gmail.com>
2021-03-10 09:11:44 -0600

    cambio de version

--- invoice-manager-ui/src/app/@theme/components/header/header.component.ts
+++ invoice-manager-ui/src/app/@theme/components/header/header.component.ts
@@ -39,7 +39,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   currentTheme = 'default';
 
-  userMenu = [{ title: 'V2.014' }];
+  userMenu = [{ title: '2.015' }];
 
   constructor(private sidebarService: NbSidebarService,
     private menuService: NbMenuService,
